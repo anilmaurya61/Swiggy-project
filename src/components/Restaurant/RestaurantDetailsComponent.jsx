12f03ad39f9c56a6961b5c29ff419f14166faab3
anilmaurya61 <anilkumarmaurya246@gmail.com>
2023-12-08 14:40:51 +0530

    Added Signout button

--- src/components/Restaurant/RestaurantDetailsComponent.jsx
+++ src/components/Restaurant/RestaurantDetailsComponent.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Button } from '@mui/material';
+import { Logout as LogoutIcon } from '@mui/icons-material'
+import {signOutUser} from '../../firebase/firebaseConfig'
+import { Navigate, useNavigate } from 'react-router-dom';
 
 const RestaurantDetailsComponent = () => {
+
+  const navigate = useNavigate();
+
+  const handleSignout = async() =>{
+    await signOutUser();
+    navigate('/restaurant/register');
+  }
   return (
-    <div>
-      <h2>Restaurant Details Component</h2>
-      {/* Add your Restaurant Details component content here */}
-    </div>
+    <>
+      <div>
+        <h2>Restaurant Details </h2>
+      </div>
+      <Button onClick={handleSignout} variant="outlined" endIcon={<LogoutIcon />}>
+        SignOut
+      </Button>
+    </>
   );
 };
 
