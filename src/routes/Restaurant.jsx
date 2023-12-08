100c17face635fb7ab9e8e64a1ba6f0e44c12677
anilmaurya61 <anilkumarmaurya246@gmail.com>
2023-12-08 11:01:35 +0530

    Updated

--- src/routes/Restaurant.jsx
+++ src/routes/Restaurant.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import RestaurantHome from '../pages/restaurent/RestaurentHome';
-import RestaurantDetails from '../pages/restaurent/RegisterDetails';
-import RestaurantRegister from '../pages/restaurent/RestrauntRegister';
+import RestaurantHome from '../pages/restaurant/RestaurentHome';
+import RestaurantDetails from '../pages/restaurant/RegisterDetails';
+import RestaurantRegister from '../pages/restaurant/RestrauntRegister';
 
 const Restaurant = () => {
   return (
