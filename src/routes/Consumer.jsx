5527c3846d03f21989c41f7443ddc38a35ffd22f
anilmaurya61 <anilkumarmaurya246@gmail.com>
2023-12-09 20:04:16 +0530

    Added Landingpage.jsx

--- src/routes/Consumer.jsx
+++ src/routes/Consumer.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ConsumerHome from '../pages/consumer/Home';
+import LandingPage from '../pages/consumer/LandingPage'
 
 const Consumer = () => {
   return (
     <>
         <Routes>
+          <Route path="/auth" element={<LandingPage />} />
           <Route path="/" element={<ConsumerHome />} />
         </Routes>
     </>
