a292cf6d89b53303e2becfbc137bfdb3d67ec940
anilmaurya61 <anilkumarmaurya246@gmail.com>
2023-12-12 12:40:00 +0530

    resolve git

--- src/app/store.js
+++ src/app/store.js
@@ -1,14 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; 
-
 import { combineReducers } from 'redux'; 
 import { AddItemsReducer, ordersReducer } from '../feature/restaurant/RestaurantHomeSlice';
 import { menuItemsApi } from '../firebase/firebaseRTKquery';
 import restaurantDetailsReducer from '../feature/restaurant/RestaurantDetailsSlice';
 import { RestaurantsApi } from '../firebase/firebaseRTKqueryRestaurants';
 import cartReducer from '../feature/consumer/CartSlice';
-import { addressesApigit  } from '../firebase/getAddressRTKquery';
+import { addressesApi } from '../firebase/getAddressRTKquery';
 
 const persistConfig = {
   key: 'root',
